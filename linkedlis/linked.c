0f3b228e9a11f0f514ace0011a406d23d2173dd5
mafika <rudolphmafika@gmail.com>
2023-04-30 16:56:26 +0000

    wrote function to create linked list node

--- linkedlis/linked.c
+++ linkedlis/linked.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int val;
+	struct node *next;
+}node;
+
+struct node *create_node(struct node *newnode, int num)/*create new node and assig value*/
+{
+	struct node *temp;
+
+	temp = malloc(sizeof(node));
+	newnode = malloc(sizeof(node));
+	
+	newnode->val = num;
+	newnode->next = temp;
+	temp->next = NULL;
+
+	return (newnode);
+}
 
 int main(void)
 {
+	struct node *head;
+
+	int i = 2;
+
+	for (; i < 21; i += 2)
+		printf("%d\n", create_node(head, i)->val);
+
 	return (0);
-	
 }
